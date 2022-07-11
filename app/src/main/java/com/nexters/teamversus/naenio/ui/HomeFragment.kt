27b35763f6dfd13476048b997ced66d24365d499
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-07-12 02:12:43 +0900

    KakaoLoginSdk 래핑한 유틸 함수 추가

--- app/src/main/java/com/nexters/teamversus/naenio/ui/HomeFragment.kt
+++ app/src/main/java/com/nexters/teamversus/naenio/ui/HomeFragment.kt
@@ -8,13 +8,18 @@ import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
-import com.kakao.sdk.user.UserApiClient
+import androidx.lifecycle.lifecycleScope
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.nexters.teamversus.naenio.extensions.fragmentComposeView
+import com.nexters.teamversus.naenio.utils.loginWithKakao
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -33,11 +38,16 @@ class HomeFragment : Fragment() {
     }
 
     private fun loginKakao() {
-        UserApiClient.instance.loginWithKakaoTalk(requireActivity()) { token, error ->
-            if (error != null) {
-                Log.e("###", "로그인 실패", error)
-            } else if (token != null) {
-                Log.i("###", "로그인 성공 ${token.accessToken}")
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                val token = loginWithKakao(requireActivity())
+                Log.d("### kakao token", "$token")
+            } catch (e: Exception) {
+                if (e is ClientError && e.reason == ClientErrorCause.Cancelled) {
+                    Log.d("MainActivity", "사용자가 명시적으로 취소")
+                } else {
+                    Log.e("MainActivity", "인증 에러 발생", e)
+                }
             }
         }
     }
@@ -49,7 +59,7 @@ fun HomeScreen(
     onClick: () -> Unit
 ) {
     Column(modifier = Modifier.background(Color.LightGray)) {
-        Button(onClick = {
+        Button(colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow), onClick = {
             onClick.invoke()
         }) {
 
