c342646dc1c432e91546d63f3f4bc6999628fdca
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-07-11 23:45:09 +0900

    initial commit

---
+++ app/src/main/java/com/nexters/teamversus/naenio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nexters.teamversus.naenio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
