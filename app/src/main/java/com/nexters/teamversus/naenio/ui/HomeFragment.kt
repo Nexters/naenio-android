dc9d0eae32c5f2cd3d19f9cb9d4e4b29815a2229
yunakim2 <98_02@naver.com>
2022-07-19 01:26:25 +0900

    Google Login 기능 구현

--- app/src/main/java/com/nexters/teamversus/naenio/ui/HomeFragment.kt
+++ app/src/main/java/com/nexters/teamversus/naenio/ui/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.nexters.teamversus.naenio.ui
 
+
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
@@ -15,6 +18,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.nexters.teamversus.naenio.extensions.fragmentComposeView
@@ -23,6 +31,11 @@ import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
+    private val getGoogleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result: ActivityResult ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        handleGoogleSignInResult(task)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,9 +43,14 @@ class HomeFragment : Fragment() {
     ): View {
         return fragmentComposeView.apply {
             setContent {
-                HomeScreen {
-                    loginKakao()
-                }
+                HomeScreen (
+                    onLoginKakao = {
+                        loginKakao()
+                    },
+                    onLoginGoogle = {
+                        loginGoogle()
+                    }
+                )
             }
         }
     }
@@ -52,15 +70,40 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun loginGoogle() {
+        Log.d("MainActivity", "[loginGoogle] start")
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+        val signInIntent = googleSignInClient.signInIntent
+        getGoogleLoginResult.launch(signInIntent)
+    }
+
+    private fun handleGoogleSignInResult(completedTask : Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.result
+            Log.d("MainActivity", "[HandleGoogleSignInResult] success data :: " + account.email)
+
+        } catch (e : ApiException) {
+            Log.e("MainActivity", "[HandleGoogleSignInResult] failed code :: " + e.statusCode.toString())
+        }
+    }
 }
 
 @Composable
 fun HomeScreen(
-    onClick: () -> Unit
+    onLoginKakao: () -> Unit,
+    onLoginGoogle: () -> Unit
 ) {
     Column(modifier = Modifier.background(Color.LightGray)) {
         Button(colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow), onClick = {
-            onClick.invoke()
+            onLoginKakao.invoke()
+        }) {
+
+        }
+        Button(colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue), onClick = {
+            onLoginGoogle.invoke()
         }) {
 
         }
@@ -71,7 +114,10 @@ fun HomeScreen(
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen {
-
-    }
+    HomeScreen (
+        onLoginKakao = {
+        },
+        onLoginGoogle = {
+        }
+    )
 }
