59dcebaff895c48803023a8f35a97b90d8e502ec
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-07-22 01:13:54 +0900

    바텀바 및 탭별 화면 구성

--- app/src/main/java/com/nexters/teamversus/naenio/MainActivity.kt
+++ app/src/main/java/com/nexters/teamversus/naenio/MainActivity.kt
@@ -1,29 +1,108 @@
 package com.nexters.teamversus.naenio
 
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.kakao.sdk.common.util.Utility
-import com.nexters.teamversus.naenio.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.nexters.teamversus.naenio.theme.MyColors
+import com.nexters.teamversus.naenio.ui.tabs.HomeScreen
+import com.nexters.teamversus.naenio.ui.tabs.ProfileScreen
+import com.nexters.teamversus.naenio.ui.tabs.FeedScreen
+import com.nexters.teamversus.naenio.ui.tabs.model.BottomNavItem
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        getHashKey()
+        setContent {
+            MainScreen()
+        }
     }
+}
 
-    private fun getHashKey() {
-        val keyHash = Utility.getKeyHash(this)
-        Log.i("++ KeyHash" , keyHash)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController = navController) }
+    ) {
+        NavigationGraph(navController = navController)
     }
+}
 
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Profile,
+        BottomNavItem.Feed,
+    )
+    BottomNavigation(
+        backgroundColor = Color.Black,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = null
+                    )
+                },
+                selectedContentColor = MyColors.PrimaryColor,
+                unselectedContentColor = MyColors.DarkGrey,
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screenRoute,
+                onClick = {
+                    navController.navigate(item.screenRoute) {
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Home.screenRoute) {
+        composable(BottomNavItem.Home.screenRoute) {
+            HomeScreen()
+        }
+        composable(BottomNavItem.Feed.screenRoute) {
+            FeedScreen()
+        }
+        composable(BottomNavItem.Profile.screenRoute) {
+            ProfileScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationPreview() {
+    MainScreen()
 }
---
+++ app/src/main/java/com/nexters/teamversus/naenio/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.nexters.teamversus.naenio.theme
+
+import androidx.compose.material.darkColors
+import androidx.compose.ui.graphics.Color
+
+class MyColors {
+    companion object {
+        val PrimaryColor = Color(0xFFD9FF00)
+        val DarkGrey = Color(0xFF8D95A0)
+    }
+}
