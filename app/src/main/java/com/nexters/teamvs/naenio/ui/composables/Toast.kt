79e6c67f44fae7bf1b06da6b7a95304301c1766a
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-08-13 03:49:39 +0900

    이벤트 받아서 스낵바 표시되도록 구현.

---
+++ app/src/main/java/com/nexters/teamvs/naenio/ui/composables/Toast.kt
@@ -0,0 +1,53 @@
+package com.nexters.teamvs.naenio.ui.composables
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nexters.teamvs.naenio.theme.Font
+
+@Composable
+fun Toast(message: String) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color(0xff313643), shape = RoundedCornerShape(10.dp))
+            .border(
+                BorderStroke(
+                    1.dp,
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xffd669ff),
+                            Color(0xff5862ff),
+                            Color(0xff0bb9ff)
+                        )
+                    )
+                )
+            )
+            .padding(16.dp),
+        text = message,
+        textAlign = TextAlign.Center,
+        style = Font.pretendardRegular16,
+        color = Color.White
+    )
+
+}
+
+@Preview
+@Composable
+fun ToastPreview() {
+    Toast(message = "123123")
+}
--- app/src/main/java/com/nexters/teamvs/naenio/ui/feed/FeedViewModel.kt
+++ app/src/main/java/com/nexters/teamvs/naenio/ui/feed/FeedViewModel.kt
@@ -1,11 +1,12 @@
 package com.nexters.teamvs.naenio.ui.feed
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nexters.teamvs.naenio.base.BaseViewModel
 import com.nexters.teamvs.naenio.domain.model.Post
 import com.nexters.teamvs.naenio.domain.repository.FeedRepository
+import com.nexters.teamvs.naenio.ui.model.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -19,6 +20,8 @@ class FeedViewModel @Inject constructor(
     private val _posts = MutableStateFlow<List<Post>>(emptyList())
     val posts = _posts.asStateFlow()
 
+    val uiState = MutableSharedFlow<UiState>()
+
     init {
         getFeedPosts()
     }
@@ -26,12 +29,17 @@ class FeedViewModel @Inject constructor(
     fun getFeedPosts() {
         viewModelScope.launch {
             try {
+                uiState.emit(UiState.Loading)
                 _posts.value = feedRepository.getFeedPosts(
                     pageSize = 10,
                     lastPostId = null
                 )
+                uiState.emit(UiState.Success)
             } catch (e: Exception) {
                 e.printStackTrace()
+                uiState.emit(UiState.Error(e))
+            } finally {
+//                uiState.emit(UiState.Success)
             }
         }
     }
