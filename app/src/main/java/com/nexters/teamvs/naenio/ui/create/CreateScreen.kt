885d570e03c2f2cad1150dc58ddadd1bf9f39788
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-08-17 00:51:18 +0900

    [Create] SoftInputMode AdjustPan 설정
    게시물 등록 화면 진입 시 > SoftInputMode AdjustPan 설정
    
    게시물 등록 화면 탈출 시 > SoftInputMode AdjustNoting으로 원복
    
    + CreateScreen 내 컴포저블들 레이지컬럼으로 구성.(화면이 작은 디바이스에서 스크롤 가능하게 함.)

--- app/src/main/java/com/nexters/teamvs/naenio/ui/create/CreateScreen.kt
+++ app/src/main/java/com/nexters/teamvs/naenio/ui/create/CreateScreen.kt
@@ -1,10 +1,13 @@
 package com.nexters.teamvs.naenio.ui.create
 
+import android.content.Context
 import android.util.Log
+import android.view.WindowManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -12,14 +15,23 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavHostController
 import com.nexters.teamvs.naenio.R
+import com.nexters.teamvs.naenio.extensions.requireActivity
 import com.nexters.teamvs.naenio.graphs.Graph
 import com.nexters.teamvs.naenio.theme.Font.montserratBold18
 import com.nexters.teamvs.naenio.theme.Font.montserratMedium12
@@ -38,8 +50,28 @@ import com.nexters.teamvs.naenio.theme.MyColors
 @Composable
 fun CreateScreen(
     navController: NavHostController,
-    viewModel: CreateViewModel = hiltViewModel()
+    viewModel: CreateViewModel = hiltViewModel(),
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    context: Context = LocalContext.current
 ) {
+
+    context.requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            Log.d("### CreateScreen", " DisposableEffect Event: $event")
+            if (event == Lifecycle.Event.ON_STOP) {
+                context.requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
     var title by remember { mutableStateOf("") }
     var voteOption1 by remember { mutableStateOf("") }
     var voteOption2 by remember { mutableStateOf("") }
@@ -71,35 +103,44 @@ fun CreateScreen(
             .padding(horizontal = 20.dp)
             .fillMaxSize()
     ) {
-        Column {
-            CreateTopBar(enabled = enabled, upload = {
-                viewModel.createPost(
-                    title = title,
-                    choices = arrayOf(voteOption1, voteOption2),
-                    content = content,
-                )
-            })
-            Spacer(modifier = Modifier.height(28.dp))
+        LazyColumn {
+            item {
+                CreateTopBar(enabled = enabled, upload = {
+                    viewModel.createPost(
+                        title = title,
+                        choices = arrayOf(voteOption1, voteOption2),
+                        content = content,
+                    )
+                })
+                Spacer(modifier = Modifier.height(28.dp))
 
-            VoteTopicInput(title) {
-                title = it
             }
-            Spacer(modifier = Modifier.height(20.dp))
+            item {
 
-            VoteOptionsInput(
-                voteOption1 = voteOption1,
-                voteOption2 = voteOption2,
-                onValueChange1 = {
-                    voteOption1 = it
-                },
-                onValueChange2 = {
-                    voteOption2 = it
+                VoteTopicInput(title) {
+                    title = it
                 }
-            )
-            Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            item {
 
-            VoteContentInput(content) {
-                content = it
+                VoteOptionsInput(
+                    voteOption1 = voteOption1,
+                    voteOption2 = voteOption2,
+                    onValueChange1 = {
+                        voteOption1 = it
+                    },
+                    onValueChange2 = {
+                        voteOption2 = it
+                    }
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+
+            item {
+                VoteContentInput(content) {
+                    content = it
+                }
             }
         }
     }
@@ -166,7 +207,9 @@ fun CreateTextField(
     Box {
         TextField(
             shape = RoundedCornerShape(8.dp),
-            modifier = modifier,
+            modifier = modifier.onFocusChanged {
+                Log.d("### onFocusChanged", "${it.hasFocus} ${it.isFocused} ${it.isCaptured}")
+            },
             textStyle = pretendardMedium16,
             colors = TextFieldDefaults.textFieldColors(
                 textColor = Color.White,
@@ -182,10 +225,13 @@ fun CreateTextField(
                     text = hint
                 )
             },
-            value = text,
+            value = TextFieldValue(
+                text = text,
+                selection = TextRange(text.length)
+            ),
             onValueChange = {
                 if (inputLength >= maxLength) return@TextField
-                onValueChange.invoke(it.replace("\n", ""))
+                onValueChange.invoke(it.text.replace("\n", ""))
             }
         )
 
