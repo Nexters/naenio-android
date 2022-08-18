61256621599edfd019ba3fc615c8eed201bf3cfa
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-08-18 23:50:42 +0900

    [Comment] 댓글 작성 날짜만 표시되게 수정

--- app/src/main/java/com/nexters/teamvs/naenio/ui/comment/CommentScreen.kt
+++ app/src/main/java/com/nexters/teamvs/naenio/ui/comment/CommentScreen.kt
@@ -398,7 +398,7 @@ fun CommentItem(
             )
             Text(
                 modifier = Modifier.padding(end = 6.dp),
-                text = comment.writeTime.toString(),
+                text = comment.writeTime.split(" ")[0], //TODO 수정하기
                 color = Color.White,
                 fontSize = 14.sp,
                 maxLines = 1
