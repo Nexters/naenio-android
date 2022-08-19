6252a5e715c1fb05fb70a02457b2cccbfc0347e3
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-08-19 23:48:56 +0900

    [로그인] 로그인 인증 플로우 수정

--- app/src/main/java/com/nexters/teamvs/naenio/ui/tabs/auth/LoginViewModel.kt
+++ app/src/main/java/com/nexters/teamvs/naenio/ui/tabs/auth/LoginViewModel.kt
@@ -15,24 +15,21 @@ import com.kakao.sdk.common.model.ClientErrorCause
 import com.nexters.teamvs.naenio.BuildConfig
 import com.nexters.teamvs.naenio.base.BaseViewModel
 import com.nexters.teamvs.naenio.base.GlobalUiEvent
-import com.nexters.teamvs.naenio.domain.repository.UserRepository
 import com.nexters.teamvs.naenio.data.network.dto.AuthType
+import com.nexters.teamvs.naenio.domain.repository.UserRepository
 import com.nexters.teamvs.naenio.extensions.errorMessage
-import com.nexters.teamvs.naenio.graphs.Route
 import com.nexters.teamvs.naenio.ui.model.User
 import com.nexters.teamvs.naenio.utils.datastore.AuthDataStore
 import com.nexters.teamvs.naenio.utils.fromJson
 import com.nexters.teamvs.naenio.utils.loginWithKakao
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 sealed class LoginDestination {
-    object ProfileSettings: LoginDestination()
-    object Main: LoginDestination()
+    object ProfileSettings : LoginDestination()
+    object Main : LoginDestination()
 }
 
 @HiltViewModel
@@ -42,32 +39,26 @@ class LoginViewModel @Inject constructor(
 
     val navigationEvent = MutableSharedFlow<LoginDestination>(extraBufferCapacity = 1)
 
-    private fun login(socialLoginToken: String, authType: AuthType) {
-        viewModelScope.launch {
-            try {
-                GlobalUiEvent.showLoading()
-                userRepository.login(socialLoginToken, authType)
-                checkProfileInfo()
-            } catch (e: Exception) {
-                GlobalUiEvent.showToast(e.errorMessage())
-            } finally {
-                GlobalUiEvent.hideLoading()
-            }
-        }
+    private suspend fun login(socialLoginToken: String, authType: AuthType) {
+        userRepository.login(socialLoginToken, authType)
     }
 
     fun loginKakao(context: Context) {
         viewModelScope.launch {
             try {
+//                GlobalUiEvent.showLoading() TODO 로고랑 로딩바랑 아이콘이 동일해서 어색해보이는 이슈
                 val token = loginWithKakao(context)
                 Log.d(className, "$token")
                 login(token.accessToken, AuthType.KAKAO)
+                checkProfileInfo()
             } catch (e: Exception) {
                 if (e is ClientError && e.reason == ClientErrorCause.Cancelled) {
                     Log.d(className, "사용자가 명시적으로 취소")
                 } else {
-                    Log.e(className, "인증 에러 발생", e)
+                    GlobalUiEvent.showToast(e.errorMessage())
                 }
+            } finally {
+                GlobalUiEvent.hideLoading()
             }
         }
     }
@@ -81,23 +72,44 @@ class LoginViewModel @Inject constructor(
     }
 
     fun handleGoogleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.result
-            Log.d(className, "[HandleGoogleSignInResult] success data :: " + account.email)
-            Log.d(className, "[HandleGoogleSignInResult] success id_token :: " + account.idToken)
-            if (account.idToken.isNullOrEmpty()) return
-            login(account.idToken!!, AuthType.GOOGLE)
-        } catch (e: ApiException) {
-            Log.e(className, "[HandleGoogleSignInResult] failed code :: " + e.statusCode.toString())
+        viewModelScope.launch {
+            try {
+                GlobalUiEvent.showLoading()
+                val account = completedTask.result
+                Log.d(className, "[HandleGoogleSignInResult] success data :: " + account.email)
+                Log.d(
+                    className,
+                    "[HandleGoogleSignInResult] success id_token :: ${account.idToken}"
+                )
+                login(account.idToken!!, AuthType.GOOGLE)
+                checkProfileInfo()
+            } catch (e: ApiException) {
+                GlobalUiEvent.showToast(e.errorMessage())
+                Log.e(
+                    className,
+                    "[HandleGoogleSignInResult] failed code :: " + e.statusCode.toString()
+                )
+            } catch (e: Exception) {
+                GlobalUiEvent.showToast(e.errorMessage())
+                e.printStackTrace()
+            } finally {
+                GlobalUiEvent.hideLoading()
+            }
         }
     }
 
-    fun checkProfileInfo() {
-        val user = AuthDataStore.userJson.fromJson<User>()
-        if (user == null || user.nickname.isNullOrEmpty()) {
+    private fun checkProfileInfo() {
+        try {
+            val user = AuthDataStore.userJson.fromJson<User>()
+            if (user == null || user.nickname.isNullOrEmpty()) {
+                navigationEvent.tryEmit(LoginDestination.ProfileSettings)
+            } else {
+                navigationEvent.tryEmit(LoginDestination.Main)
+            }
+        } catch (e: Exception) {
+            Log.e(className, e.stackTraceToString())
             navigationEvent.tryEmit(LoginDestination.ProfileSettings)
-        } else {
-            navigationEvent.tryEmit(LoginDestination.Main)
         }
     }
 }
+
