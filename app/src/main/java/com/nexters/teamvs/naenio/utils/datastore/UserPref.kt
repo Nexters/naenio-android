f805f1dcf8903ee99788f29cf72508b187bcbd23
haeseong-oh <haeseong.oh@naverz-corp.com>
2022-09-03 01:50:43 +0900

    유저 정보 캐싱을 위한 DataStore 추가

---
+++ app/src/main/java/com/nexters/teamvs/naenio/utils/datastore/UserPref.kt
@@ -0,0 +1,11 @@
+package com.nexters.teamvs.naenio.utils.datastore
+
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+
+data class UserPref(
+    val id: Int,
+    val authServiceType: String,
+    val nickname: String? = null,
+    val profileImageIndex: Int = 0
+)
